0038e00dc0c047e766c0a0fe43353160a256520c
Jan Piotrowski <piotrowski+github@gmail.com>
2023-08-06 20:06:59 +0000

    enable query logging

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,10 +8,10 @@ declare global {
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({ log: ['query']});
 } else {
   if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
+    global.cachedPrisma = new PrismaClient({ log: ['query']});
   }
   prisma = global.cachedPrisma;
 }
